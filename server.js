20635fdac060de7cd1531c567bd6c590dc1b1677
plaird730 <plaird730@gmail.com>
2018-07-23 18:13:16 -0400

    Added list and first active element to the list in handlebars

--- server.js
+++ server.js
@@ -20,37 +20,51 @@ app.set("view engine", "hbs");
 var articles = [{
         title: "Why your Web Development sucks",
         author: "Clic Bate",
-        link: "https://www.linkedin.com/in/paul-laird/"
+        link: "https://www.linkedin.com/in/paul-laird/",
+        timestamp: "4:00 PM ET",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum tortor tellus, maximus tincidunt erat tincidunt ultricies. Vestibulum sodales enim diam, eu ultrices quam suscipit et. In in justo tincidunt, ornare odio eget, tempus tortor. Nulla facilisi. Cras odio lacus, finibus et tincidunt vel, bibendum eget urna."
     },
     {
         title: "8 things you think you understand about Web Development but you really don't",
         author: "Clic Bate",
-        link: "https://www.linkedin.com/in/paul-laird/"
+        link: "https://www.linkedin.com/in/paul-laird/",
+        timestamp: "4:00 PM ET",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum tortor tellus, maximus tincidunt erat tincidunt ultricies. Vestibulum sodales enim diam, eu ultrices quam suscipit et. In in justo tincidunt, ornare odio eget, tempus tortor. Nulla facilisi. Cras odio lacus, finibus et tincidunt vel, bibendum eget urna."
     },
     {
         title: "6 incredible Web Development hacks",
         author: "Clic Bate",
-        link: "https://www.linkedin.com/in/paul-laird/"
+        link: "https://www.linkedin.com/in/paul-laird/",
+        timestamp: "4:00 PM ET",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum tortor tellus, maximus tincidunt erat tincidunt ultricies. Vestibulum sodales enim diam, eu ultrices quam suscipit et. In in justo tincidunt, ornare odio eget, tempus tortor. Nulla facilisi. Cras odio lacus, finibus et tincidunt vel, bibendum eget urna."
     },
     {
         title: "6 reasons to be addicted to JavaScript",
         author: "Clic Bate",
-        link: "https://www.linkedin.com/in/paul-laird/"
+        link: "https://www.linkedin.com/in/paul-laird/",
+        timestamp: "4:00 PM ET",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum tortor tellus, maximus tincidunt erat tincidunt ultricies. Vestibulum sodales enim diam, eu ultrices quam suscipit et. In in justo tincidunt, ornare odio eget, tempus tortor. Nulla facilisi. Cras odio lacus, finibus et tincidunt vel, bibendum eget urna."
     },
     {
         title: "What the government doesn't want you to know about JavaScript",
         author: "Clic Bate",
-        link: "https://www.linkedin.com/in/paul-laird/"
+        link: "https://www.linkedin.com/in/paul-laird/",
+        timestamp: "4:00 PM ET",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum tortor tellus, maximus tincidunt erat tincidunt ultricies. Vestibulum sodales enim diam, eu ultrices quam suscipit et. In in justo tincidunt, ornare odio eget, tempus tortor. Nulla facilisi. Cras odio lacus, finibus et tincidunt vel, bibendum eget urna."
     },
     {
         title: "Why JavaScript is destroying America",
         author: "Clic Bate",
-        link: "https://www.linkedin.com/in/paul-laird/"
+        link: "https://www.linkedin.com/in/paul-laird/",
+        timestamp: "4:00 PM ET",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum tortor tellus, maximus tincidunt erat tincidunt ultricies. Vestibulum sodales enim diam, eu ultrices quam suscipit et. In in justo tincidunt, ornare odio eget, tempus tortor. Nulla facilisi. Cras odio lacus, finibus et tincidunt vel, bibendum eget urna."
     },
     {
         title: "5 BS facts about Web Development that everyone thinks are true",
         author: "Clic Bate",
-        link: "https://www.linkedin.com/in/paul-laird/"
+        link: "https://www.linkedin.com/in/paul-laird/",
+        timestamp: "4:00 PM ET",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum tortor tellus, maximus tincidunt erat tincidunt ultricies. Vestibulum sodales enim diam, eu ultrices quam suscipit et. In in justo tincidunt, ornare odio eget, tempus tortor. Nulla facilisi. Cras odio lacus, finibus et tincidunt vel, bibendum eget urna."
     }
 ];
 
